4c8d5f6a5ac2f7637591ced3ea797314dc52040b
Stephen Estrada <stcrestrada@gmail.com>
2016-10-03 20:30:03 -0400

    initial setup, function findGap working, first commit :

---
+++ src/dateGap.js
@@ -0,0 +1,26 @@
+'use strict';
+var moment = require('moment');
+
+
+exports.findGap = function (srvcDt, gap = 10) {
+    var serviceDate = parseDate(srvcDt)
+    var daysAgo = configureNumberOfDays(gap)
+    var dt = currentDateFormatted()
+    if (daysAgo <= serviceDate && serviceDate <= dt) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function parseDate(date) {
+    var parsedDate = new Date(date);
+    return moment.parseZone(parsedDate).utc().format('L');
+}
+
+function configureNumberOfDays(gap) {
+    return moment().subtract(gap, 'days').calendar()
+}
+function currentDateFormatted() {
+    return moment().format('L')
+}
