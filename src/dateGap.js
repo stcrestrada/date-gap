2ac08ce38b0ded5139a0c2f9372f9039f2a55b11
Stephen Estrada <stcrestrada@gmail.com>
2016-10-06 12:46:04 -0400

    update code to be compatible with es5

--- src/dateGap.js
+++ src/dateGap.js
@@ -2,25 +2,26 @@
 var moment = require('moment');
 
 
-module.exports.findGap = function (srvcDt, gap = 10) {
-    var serviceDate = parseDate(srvcDt)
-    var daysAgo = configureNumberOfDays(gap)
-    var dt = currentDateFormatted()
-    if (daysAgo <= serviceDate && serviceDate <= dt) {
-        return true
-    } else {
-        return false
-    }
+function findGap(srvcDt) {
+  var gap = 10
+  var dt = currentDateFormatted()
+  var serviceDate = parseDate(srvcDt)
+  var daysAgo = configureNumberOfDays(gap)
+  if (daysAgo <= serviceDate && serviceDate <= dt) {
+    return true
+  } else {
+    return false
+  }
 }
 
 function parseDate(date) {
-    var parsedDate = new Date(date);
-    return moment.parseZone(parsedDate).utc().format('L');
+  var parsedDate = new Date(date);
+  return moment.parseZone(parsedDate).utc().format('L');
 }
 
 function configureNumberOfDays(gap) {
-    return moment().subtract(gap, 'days').calendar()
+  return moment().subtract(gap, 'days').calendar()
 }
 function currentDateFormatted() {
-    return moment().format('L')
+  return moment().format('L')
 }
